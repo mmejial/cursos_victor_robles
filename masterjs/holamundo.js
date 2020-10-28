3b773d0eb1c13e152ae16ffe5465e3089ee318e0
sheevarip <tomatin77@gmail.com>
2020-10-27 23:20:19 -0600

    sass

---
+++ masterjs/holamundo.js
@@ -0,0 +1,32 @@
+'use strict'
+
+//pedir dos numeros y compararlos/* 
+/*  */
+/* var a;
+var b;
+
+a = prompt('intruduce un numero');
+b = prompt('introduce otro numero');
+
+while (a<=0 || b<=0  || isNaN(a) || isNaN(b)){ isNaN es para ver si es un numero  
+    a = prompt('intruduce un numero mayo a 0 o que no sea letra');
+b = prompt('introduce un numero ');
+
+}
+if (a == b){
+    console.log('son iguales');
+}
+else if (a<b){
+    console.log(`el nummero ${a} es menor que ${b}`);
+}
+else{
+    console.log('el numero '+b+' es menor que '+a);
+}
+ */
+
+ var a=0;
+ var b=0;
+
+ do{
+    
+ }while( a < 0)

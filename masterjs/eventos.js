8e19bf16552413783b247d1bc79c847067358cca
Marco <tomatin77@gmail.com>
2021-03-23 18:59:05 -0600

    curso yt

--- masterjs/eventos.js
+++ masterjs/eventos.js
@@ -1,43 +1,17 @@
-let boton = document.querySelector("#boton");
 
-function cambiarColor() {
-    console.log("object");
-    let bg = boton.style.background;
-    if (bg == "green") {
-        boton.style.background = "red";
-    } else {
-        boton.style.background = "green";
-    }
+document.addEventListener('DOMContentLoaded', function(){
+    
 
-}
-//evento click
-boton.addEventListener('click', function() {
-    cambiarColor();
+const prueba1= document.getElementById('pruebah1')
+const seccion = document.getElementById('seccion')
+
+
+prueba1.innerText='yo soy el pipiripao';
+
+    //keydown
+
+
+    //keyup
 });
 
-//mouse over
-boton.addEventListener('mouseover', function() {
-    console.log('tachas');
-})
 
-//mouse out
-boton.addEventListener('mouseout', function() {
-    console.log('y perico');
-})
-
-//focus
-let input = document.querySelector('#campoNombre');
-input.addEventListener('focus', function() {
-    console.log('y focus');
-})
-
-//blur
-input.addEventListener('blur', function() {
-    console.log('y blur');
-})
-
-
-//keydown
-
-
-//keyup
---
+++ masterjs/timesr.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', () => {
+
+    var tiempo = setInterval(function() {
+        document.querySelector("h1").style.fontSize = "50px";
+    }, 3000)
+
+
+});
